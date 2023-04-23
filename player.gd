1791e390b184424537bb66550becebcd89f3a0c5
tym <xxtymmie@gmail.com>
2023-04-23 14:57:26 -0500

    Redid player animation and sprite to use AnimationPlayer
    AnimationPlayer is an easier way to create animations  instead of AnimatedSprite2D

--- player.gd
+++ player.gd
@@ -1,48 +1,33 @@
 extends CharacterBody2D
 
-const SPEED = 100
-const ATTACK_DURATION = 0.5
+@export var speed: int = 35
+@onready var animations = $AnimationPlayer
 
-var input_direction: get = _get_input_direction
-var sprite_direction = "Down": get =_get_sprite_direction
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * speed
 
-@onready var sprite = $AnimatedSprite2D
-var attack_timer = 0.5
 
-func _get_input_direction():
-	@warning_ignore("shadowed_variable")
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
-	return input_direction
-
-func _physics_process(delta):
-	if attack_timer > 0.0:
-		attack_timer -= delta
-		if attack_timer <= 0.0:
-			set_animation("Walk")
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
 	else:
-		velocity = input_direction * SPEED
-		move_and_slide()
-		set_animation("Walk")
-		if velocity == Vector2.ZERO:
-			sprite.stop()
-		
-	set_animation("Walk")
-	if velocity == Vector2.ZERO:
-		sprite.stop()
+		var direction = ""
+		if velocity.x < 0: 
+			direction = "Left"
+			$Sprite2D.flip_h = true #flip animation horizontally
+		elif velocity.x > 0: 
+			direction = "Right"
+			$Sprite2D.flip_h = false #reset flip
+		elif velocity.y < 0: 
+			direction = "Up"
+		elif velocity.y > 0: 
+			direction = "Down"
+	
+		animations.play("Walk" + direction)
+	
 
-func _get_sprite_direction():
-	match input_direction:
-		Vector2.LEFT:
-			sprite_direction = "Left"
-		Vector2.RIGHT:
-			sprite_direction = "Right"
-		Vector2.UP:
-			sprite_direction = "Up"
-		Vector2.DOWN:
-			sprite_direction = "Down"
-	return sprite_direction
-
-func set_animation(animation):
-	var direction = "Side" if sprite_direction in ["Left", "Right"] else sprite_direction
-	sprite.play(animation + direction)
-	sprite.flip_h = (sprite_direction == "Left")
+func _physics_process(_delta):
+	handleInput()
+	move_and_slide()
+	updateAnimation()
