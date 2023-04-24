9b2db035b4257a4bc060c8a1ecf5e9237139cd07
danny <dannynannijr@gmail.com>
2023-04-23 17:35:28 -0700

    Player Movement & Animation Update
    - Fixed a bug where player movement animations lagged on a quick button press. Fixed this by updating the movement script to use "action_pressed" methods instead of checking velocities of the player in whatever direction and then forcing the animation player to advance to frame 1 on action_press.
    - Created a simple 2D camera that now follows the player around so I stop walking off the screen :)

--- player.gd
+++ player.gd
@@ -1,31 +1,48 @@
 extends CharacterBody2D
 
-@export var speed: int = 35
+@export var speed: float = 70
 @onready var animations = $AnimationPlayer
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
+	print(velocity.x)
 
 
 func updateAnimation():
-	if velocity.length() == 0:
-		animations.stop()
-	else:
-		var direction = ""
-		if velocity.x < 0: 
-			direction = "Left"
-			$Sprite2D.flip_h = true #flip animation horizontally
-		elif velocity.x > 0: 
-			direction = "Right"
-			$Sprite2D.flip_h = false #reset flip
-		elif velocity.y < 0: 
-			direction = "Up"
-		elif velocity.y > 0: 
-			direction = "Down"
-	
+	if Input.is_action_just_pressed("ui_right"):
+		var direction = "Right"
+		$Sprite2D.flip_h = false #reset flip
 		animations.play("Walk" + direction)
+		animations.advance(1)
+		
+	elif Input.is_action_just_released("ui_right"):
+		animations.stop()
+		
+	if Input.is_action_just_pressed("ui_left"):
+		var direction = "Left"
+		$Sprite2D.flip_h = true #reset flip
+		animations.play("Walk" + direction)
+		animations.advance(1)
+		
+	elif Input.is_action_just_released("ui_left"):
+		animations.stop()
 	
+	if Input.is_action_just_pressed("ui_up"):
+		var direction = "Up"
+		animations.play("Walk" + direction)
+		animations.advance(1)
+		
+	elif Input.is_action_just_released("ui_up"):
+		animations.stop()
+		
+	if Input.is_action_just_pressed("ui_down"):
+		var direction = "Down"
+		animations.play("Walk" + direction)
+		animations.advance(1)
+		
+	elif Input.is_action_just_released("ui_down"):
+		animations.stop()
 
 func _physics_process(_delta):
 	handleInput()
